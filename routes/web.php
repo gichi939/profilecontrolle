81e0c3f8c01fbb1df001c6663cc3bac038e52c78
”名前” <>
2020-05-14 08:08:16 +0000

    Routing

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create',
+    'admin\newsController@add');
+});
+Route::get('×××',
+'bbb\AAAController@add');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create',
+    'Admin\ProfileController@add');
+    
+    Route::get('profile/edit',
+    'Admin\ProfileController@edit');
+});
